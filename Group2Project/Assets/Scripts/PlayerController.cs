1504a6a4f03541fc40ef8ccc21a7312f537dd46b
melwells <55035851+melwells@users.noreply.github.com>
2020-02-05 18:47:21 -0500

    Proto Start
    Prototype started. Only movement attached to player character done.

---
+++ Group2Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+  public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
